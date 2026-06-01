3f82063de42a2936b1ad33c1a7e10bc2f30fb881
ltg0829 <71542896+ltg0829@users.noreply.github.com>
2026-06-01 15:28:55 +0900

    update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+const supabaseUrl = https://yuerrwfwfscrujkidmyc.supabase.co || ''
+const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1ZXJyd2Z3ZnNjcnVqa2lkbXljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzOTEzMjYsImV4cCI6MjA5Mzk2NzMyNn0.P2_rqPmgnV3YYN5NA1GG0PfaKahWJEcNVLbAzbzoEb4 || ''
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
